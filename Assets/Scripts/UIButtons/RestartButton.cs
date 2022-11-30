b64c3bf61d3a67b2969fe701156130fc18d35bf6
Фёдор <fedpr.pavlov.02@mail.ru>
2022-11-30 17:54:51 +0700

    Added logic

--- Assets/Scripts/UIButtons/RestartButton.cs
+++ Assets/Scripts/UIButtons/RestartButton.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RestartButton : MonoBehaviour
 {
     public void RestartLevel()
     {
-        print("Add logic!!!");
+        SceneManager.LoadScene("GameOverScene");
     }
 }
