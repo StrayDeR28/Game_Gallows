c2775612aa2fc97baccf8f7e6f247a657edec5ab
NiZoX <104158784+NiZoX101@users.noreply.github.com>
2022-12-14 18:22:12 +0700

    Changed the  appeareance of menu, changed in the statistics menu
    Изменён внешний вид выбора режимов и уровней сложности, добавлена кнопка обнуления статистики с функционалом.

--- Assets/Scripts/MainMenu/StatisticsScripts/Statistics.cs
+++ Assets/Scripts/MainMenu/StatisticsScripts/Statistics.cs
@@ -22,9 +22,10 @@ public class Statistics : MonoBehaviour
     {
         int wins = PlayerPrefs.GetInt("GameWon");
         int defeats = PlayerPrefs.GetInt("GameLost");
-        //_wins.GetComponent<Text>().text = ("Победы: " + PlayerPrefs.GetInt("GameWon").ToString());//обращаемся к хранящимся в PlayerPrefs данным и выводим их
-        //_defeats.GetComponent<Text>().text = ("Поражения: " + PlayerPrefs.GetInt("GameLost").ToString());
-        //_wdCompare.GetComponent<Text>().text = ("Соотношение Побед/поражений: " + Mathf.Round(wins*100/defeats)*0.01);
+        _wins.GetComponent<Text>().text = ("Победы: " + PlayerPrefs.GetInt("GameWon").ToString());//обращаемся к хранящимся в PlayerPrefs данным и выводим их
+        _defeats.GetComponent<Text>().text = ("Поражения: " + PlayerPrefs.GetInt("GameLost").ToString());
+        if (defeats != 0)
+        { _wdCompare.GetComponent<Text>().text = ("Соотношение Побед/поражений: " + Mathf.Round(wins * 100 / defeats) * 0.01); }
         //_gameWord.GetComponent<Text>().text = ("Слово:" + PlayerPrefs.GetString("GameWord"));
         //_gameSection.GetComponent<Text>().text = ("Раздел:" + PlayerPrefs.GetString("GameSection"));
         //_lastGameResult.GetComponent<Text>().text = (" ") + PlayerPrefs.GetString("LastGameResult");
@@ -33,5 +34,7 @@ public class Statistics : MonoBehaviour
     public void ResetStatistic()//Сделать кнопкой в статистике
     {
         PlayerPrefs.DeleteAll();
+        ShowStatistic();
+
     }
 }
